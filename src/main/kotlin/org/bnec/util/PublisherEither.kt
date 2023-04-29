52b9223ba1873be00051d5a0a4c1125a4b8e41ad
San <istamarahsan@gmail.com>
2023-04-29 12:40:47 +0800

    refactor Verify `handle()` 2x

--- src/main/kotlin/org/bnec/util/PublisherEither.kt
+++ src/main/kotlin/org/bnec/util/PublisherEither.kt
@@ -1,8 +1,12 @@
 package org.bnec.util
 
 import arrow.core.Either
+import arrow.core.flatMap
 import arrow.core.left
 import reactor.core.publisher.Mono
+
+inline fun <Left, LeftA : Left, LeftB : Left, A, B> Mono<Either<LeftA, A>>.mapEither(crossinline f: (A) -> Either<LeftB, B>): Mono<Either<Left, B>> =
+  this.map { it.flatMap(f) }
 
 inline fun <Left, LeftA : Left, LeftB : Left, A, B> Mono<Either<LeftA, A>>.flatMapEither(crossinline f: (A) -> Mono<Either<LeftB, B>>): Mono<Either<Left, B>> =
   this.flatMap { it.fold(ifLeft = { a -> Mono.just(a.left()) }, ifRight = f) }
